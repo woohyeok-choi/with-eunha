287353518066e4c4fdefa1c3e16d30c2510d1aa8
Woohyeok Choi <keltpower0@gmail.com>
2025-12-27 11:03:45 +0900

    intermediate

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,22 +1,43 @@
 import type { GatsbyConfig } from "gatsby";
 
+
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `with-eunha`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `최우혁 ♥ 강은하 결혼합니다!`,
+    siteUrl: `https://woohyeok-choi.github.io/with-eunha/`,
+    image: `./resources/main.png`,
+    description: `2026년 6월 13일 토요일 오후 1시 서울대학교 교수회관 웨딩홀`,
+    locale: "ko-KR",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }],
+  plugins: [
+      "gatsby-plugin-postcss",
+      "gatsby-plugin-image",
+      "gatsby-plugin-sharp",
+      "gatsby-transformer-sharp",
+      {
+          resolve: 'gatsby-source-filesystem',
+          options: {
+              "name": "resources",
+              "path": "./src/resources/"
+          },
+          __key: "resources"
+      },
+      {
+          resolve: "gatsby-plugin-manifest",
+          options: {
+              icon: "./src/resources/logo.png",
+              name: "최우혁 ♥ 강은하 결혼합니다!",
+              short_name: "ㅇㅎ ♥ ㅇㅎ",
+              start_url: "/",
+          }
+      }
+
+  ],
+
   pathPrefix: "/with-eunha",
 };
 
---
+++ gatsby-node.ts
@@ -0,0 +1,14 @@
+import * as path from "path"
+
+
+// @ts-ignore
+export const onCreateWebpackConfig = ({ actions }) => {
+    actions.setWebpackConfig({
+        resolve: {
+            alias: {
+                "@/components": path.resolve(__dirname, "src/components"),
+                "@/lib/utils": path.resolve(__dirname, "src/lib/utils"),
+            },
+        },
+    })
+}
